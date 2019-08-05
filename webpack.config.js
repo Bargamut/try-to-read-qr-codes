d15cca5043c29759fd0b6ef1ff4d37be8bd357e6
Paul Petrov <paul@bargamut.ru>
2019-08-05 04:54:21 +0300

    Install Webpack & make Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+	entry: `./src/index.js`,
+	output: {
+		filename: `main.js`,
+		path: path.join(__dirname, `./app`),
+		publicPath: `app/`
+	},
+	devServer: {
+		overlay: true,
+		contentBase: path.join(__dirname, `./app`),
+		compress: false,
+		historyApiFallback: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
